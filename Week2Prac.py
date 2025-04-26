bbc4e07c1aec10b8946dc4ef935ae58c5f9a72ae
Jake Rohan <jake.rohan@gmail.com>
2025-04-26 10:09:26 +0930

    saving for pc restart

--- Week2Prac.py
+++ Week2Prac.py
@@ -17,3 +17,24 @@ class Bike:
                 print(f"Gear decreased to: {self.gear}")
             else:
                 print("Gear could not be changed.")
+
+class Cyclist:
+    def __init__(self):
+        self.name = "Jake Rohan"
+        self.age = 35
+        self.weight = 105
+        self.proficiency = "Mountain"
+        self.protective_gear = False
+
+    def accelerate(self):
+        print("Got to go fast.")
+
+    def brake(self):
+        print("Stopping!")
+
+    def turn(self, direction):
+        print(f"Turning {direction}")
+
+    def wear_protective_gear(self):
+        self.protective_gear = True
+        print("Protective gear is now on.")
